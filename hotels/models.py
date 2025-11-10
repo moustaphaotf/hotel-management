e1ccb9566d6aceb35a6c2f159ddbc7a1c1226096
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2025-11-10 12:57:12 +0000

    feature: created the Hotel model

--- hotels/models.py
+++ hotels/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Hotel(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="hotels",
+        on_delete=models.RESTRICT,
+    )
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3, default="GNF")
+    image = models.ImageField(upload_to="hotels/", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.address}"
---
+++ hotels/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Hotel
+
+class HotelSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = Hotel
+        fields = [
+            "id",
+            "owner",
+            "name",
+            "address",
+            "email",
+            "phone",
+            "price_per_night",
+            "currency",
+            "image",
+            "created_at",
+            "updated_at",
+        ]
